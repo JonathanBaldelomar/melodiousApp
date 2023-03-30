5dde2f589a8ce02e93cd54e1c21d165825ac15cd
”Andres <Andres.Mamani@jala.university>
2023-03-30 16:22:28 -0400

    DBContext was added in DataAccess

---
+++ MelodiousApp/MelodiousApp.DataAccess/Persistence/MelodiousContext.cs
@@ -0,0 +1,23 @@
+﻿using BooksModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace MelodiousApp.DataAccess.Persistence
+{
+    public class MelodiousContext : DbContext
+    {
+        public MelodiousContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<ArtistSong> ArtistSongs { get; set; }
+        public DbSet<Credential> Credentials { get; set; }
+        public DbSet<PlayList> PlayLists { get; set; }
+        public DbSet<PlayListSong> PlayListSongs { get; set; }
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<SongAlbum> SongAlbums { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserPlayList> UserPlayLists { get; set; }
+    }
+}
